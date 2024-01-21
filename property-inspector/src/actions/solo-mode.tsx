b4e4ba3976141eb6921846a476c169a1ca57162d
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2024-01-21 10:05:08 +0100

    update solo mode settings

--- property-inspector/src/actions/solo-mode.tsx
+++ property-inspector/src/actions/solo-mode.tsx
@@ -4,7 +4,7 @@ import { useStreamDeck } from "../StreamDeck";
 import { useEffect, useState } from "react";
 
 export default () => {
-  const { openURL, globalSettings, sendToPlugin } = useStreamDeck();
+  const { openURL, os, globalSettings, sendToPlugin } = useStreamDeck();
   const { enabledSoloService = false } = globalSettings;
   const [prev, setPrev] = useState(enabledSoloService);
   const [loading, setLoading] = useState(false);
@@ -18,60 +18,74 @@ export default () => {
 
   return (
     <div>
-      <Alert
-        radius="md"
-        mb="sm"
-        icon={<IconBrandWindows size={24} />}
-        color="#222222"
-        variant="filled"
-        title="Platform"
-      >
-        <Text mt={-4} c="dimmed">
-          This action works only on <strong>Windows 10/11</strong> pc.
-        </Text>
-      </Alert>
-
-      <Divider labelPosition="center" label="Windows Service" my="sm" />
-
-      <Group mb="xs" gap={0}>
-        <Text size="sm">
-          This action is required only the first time. For more info about how
-          this "solo mode" works please see the{" "}
-        </Text>
-        <Text
-          size="sm"
-          mx={4}
-          c="dim"
-          fw="bold"
-          style={{ cursor: "pointer" }}
-          onClick={() =>
-            openURL(
-              "https://github.com/dim-stream-deck/com.dim.streamdeck/wiki/Solo-Mode-(Action)"
-            )
-          }
+      {os !== "windows" ? (
+        <Alert
+          radius="md"
+          mb="sm"
+          icon={<IconBrandWindows size={24} />}
+          color="#222222"
+          variant="filled"
+          title="Platform"
         >
-          wiki
-        </Text>
-        <Text size="sm">on GitHub.</Text>
-      </Group>
+          <Text mt={-4} c="dimmed">
+            This action works only on <strong>Windows 10/11</strong> pc.
+          </Text>
+        </Alert>
+      ) : (
+        <>
+          <Alert title="Warning" color="#222222" variant="filled">
+            <Text mt={-4} c="dimmed">
+              This actions works only if you use{" "}
+              <strong>Windows Defender</strong> as Firewall
+            </Text>
+          </Alert>
 
-      <Button
-        variant="filled"
-        color={enabledSoloService ? "#202020" : "dim"}
-        loading={loading}
-        onClick={() => {
-          setLoading(true);
-          sendToPlugin({
-            action: enabledSoloService ? "remove-service" : "install-service",
-          });
-        }}
-        fullWidth
-        leftSection={
-          enabledSoloService ? <IconX size={20} /> : <IconApps size={20} />
-        }
-      >
-        {enabledSoloService ? "Remove" : "Install"} the service
-      </Button>
+          <Divider labelPosition="center" label="Windows Service" my="sm" />
+
+          <Group mb="xs" gap={0}>
+            <Text size="sm">
+              This is required the first time only. For more info about how this
+              "solo mode" works please see the
+              <Text
+                component="span"
+                inline
+                mx={4}
+                c="dim"
+                fw="bold"
+                style={{ cursor: "pointer" }}
+                onClick={() =>
+                  openURL(
+                    "https://github.com/dim-stream-deck/com.dim.streamdeck/wiki/Solo-Mode-(Action)"
+                  )
+                }
+              >
+                wiki
+              </Text>
+              on GitHub.
+            </Text>
+          </Group>
+
+          <Button
+            variant="filled"
+            color={enabledSoloService ? "#202020" : "dim"}
+            loading={loading}
+            onClick={() => {
+              setLoading(true);
+              sendToPlugin({
+                action: enabledSoloService
+                  ? "remove-service"
+                  : "install-service",
+              });
+            }}
+            fullWidth
+            leftSection={
+              enabledSoloService ? <IconX size={20} /> : <IconApps size={20} />
+            }
+          >
+            {enabledSoloService ? "Remove" : "Install"} the service
+          </Button>
+        </>
+      )}
     </div>
   );
 };
