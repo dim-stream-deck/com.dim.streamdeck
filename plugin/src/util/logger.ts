14f373ec911e6a0898f3738e6805f6763da680e6
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2024-01-27 09:54:12 +0100

    fix logging

--- plugin/src/util/logger.ts
+++ plugin/src/util/logger.ts
@@ -2,20 +2,22 @@ import $ from "@elgato/streamdeck";
 
 export const log = (type: string, data?: Record<string, string>) => {
   $.logger.info(`action: ${type}`);
-  fetch(
-    `https://api.axiom.co/v1/datasets/${process.env.AXIOM_DATASET}/ingest`,
-    {
-      method: "POST",
-      headers: new Headers({
-        Authorization: `Bearer ${process.env.AXIOM_TOKEN}`,
-        "Content-Type": "application/json",
-      }),
-      body: JSON.stringify([
-        {
-          _time: new Date().toISOString(),
-          data: { type, data },
-        },
-      ]),
-    }
-  );
+  if (process.env.STREAM_DECK_ENV !== "develop") {
+    fetch(
+      `https://api.axiom.co/v1/datasets/${process.env.AXIOM_DATASET}/ingest`,
+      {
+        method: "POST",
+        headers: new Headers({
+          Authorization: `Bearer ${process.env.AXIOM_TOKEN}`,
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify([
+          {
+            _time: new Date().toISOString(),
+            data: { type, data },
+          },
+        ]),
+      }
+    );
+  }
 };
